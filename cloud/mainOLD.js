6ee136c222574f5c639874f7a0d71e98949837ed
Andrew Bagshaw <abagshaw@hotmail.ca>
2016-08-09 15:20:10 -0700

    firstAlpha

--- cloud/mainOLD.js
+++ cloud/mainOLD.js
@@ -96,10 +96,17 @@ function processTimes(clapTimes, fileInfo) {
     let fileIDs = fileInfo.map(function (x) { return x[2]; });
     return result;
 }
+let testClapTimes = [1469596205, 1469596221, 1469596234];
+let testFileData = [[1469595543, 3, 1], [1469595551, 7, 2], [1469595563, 11, 3], [1469595577, 1, 4], [1469595586, 9, 5], [1469595597, 11, 6], [1469595611, 3, 7], [1469596201, 9, 8], [1469596212, 1, 9], [1469596216, 12, 10], [1469596230, 4, 11], [1469596236, 5, 12]];
+let ourResult = processTimes(testClapTimes, testFileData);
+console.log(ourResult);
+console.log("yay!");
 Parse.Cloud.define("processTimes", function (request, response) {
-    let testClapTimes = [1469596205, 1469596221, 1469596234];
-    let requestFileInfo = request["data"];
-    let result = processTimes(testClapTimes, requestFileInfo);
-    response.success(result);
+    console.log(request);
+    response.success("IT WORKS!!!");
 });
+Parse.Cloud.define('hello', function(req, res) {
+  res.success('Hi');
+});
+console.log("functionsdefined!");
 //# sourceMappingURL=app.js.map

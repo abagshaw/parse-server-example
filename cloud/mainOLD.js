edce7a3c750f9c12fcb09684ce58f7f10de121f0
Andrew Bagshaw <abagshaw@hotmail.ca>
2016-08-11 22:10:21 -0700

    newMain

--- cloud/mainOLD.js
+++ cloud/mainOLD.js
@@ -1,29 +1,33 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
 function rateMatch(offset, clapDiff, fileDiff) {
-    let localClapDiff = clapDiff.map(function (x) { return x + offset; });
+    let localClapDiff = clapDiff.map(function (x) { return { "clapID": x["clapID"], "clapPoint": (x["clapPoint"] + offset) }; });
     let localFileDiff = fileDiff.slice();
     let hits = 0;
     let goodHits = 0;
     let next = -1;
     let possibleFutureHits = clapDiff.length;
     let matches = [];
-    let clapID = 0;
-    let fileID = 0;
     for (let i of localClapDiff) {
-        clapID++;
-        if (fileDiff[fileDiff.length - 1][1] < i) {
+        if (fileDiff[fileDiff.length - 1][1] < i["clapPoint"]) {
             possibleFutureHits -= 1;
             continue;
         }
         for (let j of localFileDiff.slice()) {
-            fileID++;
-            if (j[0] > i) {
+            if (j["startPoint"] > i["clapPoint"]) {
                 break;
             }
             localFileDiff.splice(localFileDiff.indexOf(j), 1);
-            if (j[0] <= i && j[1] >= i) {
-                matches.push([clapID, fileID]);
+            if (j["startPoint"] <= i["clapPoint"] && j["endPoint"] >= i["clapPoint"]) {
+                matches.push({ "clapID": i["clapID"], "fileID": j["fileID"] });
                 hits++;
-                let percentagePlace = (i - j[0]) / (j[1] - j[0]) * 100;
+                let percentagePlace = (i["clapPoint"] - j["startPoint"]) / (j["endPoint"] - j["startPoint"]) * 100;
                 if (percentagePlace < 40) {
                     goodHits++;
                 }
@@ -35,8 +39,8 @@ function rateMatch(offset, clapDiff, fileDiff) {
     localFileDiff = fileDiff.slice();
     for (let i of localClapDiff) {
         for (let j of localFileDiff.slice()) {
-            if (i < j[0] && (j[0] - i < next || next === -1)) {
-                next = j[0] - i;
+            if (i["clapPoint"] < j["startPoint"] && (j["startPoint"] - i["clapPoint"] < next || next === -1)) {
+                next = j["startPoint"] - i["clapPoint"];
                 localFileDiff.splice(localFileDiff.indexOf(j), 1);
             }
         }
@@ -45,7 +49,7 @@ function rateMatch(offset, clapDiff, fileDiff) {
 }
 function match(clapDiff, fileDiff) {
     let currentOffset = 0;
-    let bestHits = [-1, -1, -1, -1, [[-1, -1]]];
+    let bestHits = [-1, -1, -1, -1, []];
     while (true) {
         let tmpMatch = rateMatch(currentOffset, clapDiff, fileDiff);
         if (tmpMatch[0] > bestHits[0]) {
@@ -62,47 +66,87 @@ function match(clapDiff, fileDiff) {
         }
         currentOffset += tmpMatch[2];
     }
-    return [bestHits, currentOffset];
+    return [bestHits[0], bestHits[1], bestHits[4]];
 }
-function getDifferences(times) {
-    if (times.length === 0) {
+function getClapDifferences(clapInfo) {
+    if (clapInfo.length === 0) {
         return null;
     }
-    if (times[0] instanceof Array) {
-        let initialTime = times[0][0];
-        let returnTimes = [];
-        returnTimes.push([0, times[0][1] - initialTime]);
-        for (let i of times) {
-            returnTimes.push([(i[0] - initialTime), (i[1] - initialTime)]);
-        }
-        return returnTimes;
+    let initialTime = clapInfo[0]["clapTime"];
+    let returnClapInfo = [];
+    for (let i of clapInfo) {
+        returnClapInfo.push({ "clapID": i["clapID"], "clapPoint": (i["clapTime"] - initialTime) });
     }
-    else {
-        let initialTime = times[0];
-        let returnTimes = [];
-        for (let i of times) {
-            returnTimes.push(i - initialTime);
-        }
-        return returnTimes;
-    }
+    return returnClapInfo;
 }
-function processTimes(clapTimes, fileInfo) {
-    fileInfo = fileInfo.sort((n1, n2) => n1[0] - n2[0]);
-    let fileTimes = fileInfo.map(function (x) { return [x[0], x[0] + x[1]]; });
-    let clapDifferences = getDifferences(clapTimes);
-    let fileDifferences = getDifferences(fileTimes);
+function getFileDifferences(fileInfo) {
+    if (fileInfo.length === 0) {
+        return null;
+    }
+    let initialTime = fileInfo[0]["startTime"];
+    let returnData = [];
+    for (let i of fileInfo) {
+        returnData.push({ "fileID": i["fileID"], "startPoint": (i["startTime"] - initialTime), "endPoint": (i["startTime"] + i["duration"] - initialTime) });
+    }
+    return returnData;
+}
+function processTimes(clapInfo, fileInfo) {
+    fileInfo = fileInfo.sort((n1, n2) => n1["startTime"] - n2["startTime"]);
+    let clapDifferences = getClapDifferences(clapInfo);
+    let fileDifferences = getFileDifferences(fileInfo);
     let result = match(clapDifferences, fileDifferences);
-    clapDifferences = clapDifferences.map(function (x) { return x + result[1]; });
-    let fileIDs = fileInfo.map(function (x) { return x[2]; });
     return result;
 }
+
+function processPossibleClapProjs(fileData, clapDataClass) {
+    return __awaiter(this, void 0, Promise, function* () {
+        //getClapData
+        let clapData = Parse.Object.extend(clapDataClass);
+        let query = new Parse.Query(clapData);
+        let possibleProjects = yield query.find();
+        if (possibleProjects.length === 0)
+            return "!ERR!noPossibleProjects";
+        let bestProject;
+        let bestMatch;
+        for (let possibleProject of possibleProjects) {
+            let clapTimes = possibleProject.get("clapTimes");
+            let tmpMatch = processTimes(clapTimes, fileData);
+            if ((typeof bestMatch === 'undefined' || tmpMatch[0] > bestMatch[0] || (tmpMatch[0] === bestMatch[0] && tmpMatch[1] > bestMatch[1])) && tmpMatch[0] > (clapTimes.length * 0.6)) {
+                tmpMatch = bestMatch;
+                bestProject = possibleProject;
+                if (bestMatch[1] === clapTimes.length)
+                    break;
+            }
+        }
+        if (typeof bestMatch === 'undefined')
+            return "!ERR!noGoodMatches";
+        return bestMatch[2];
+    });
+}
+function processRequest(fileData, clapDataClass, response) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let result = yield processPossibleClapProjs(fileData, clapDataClass);
+            if (typeof result === 'string') {
+                response.error(result);
+                return;
+            }
+            else if (result instanceof Array) {
+                response.success(result);
+                return;
+            }
+        }
+        catch (e) {
+            response.error(e);
+            return;
+        }
+    });
+}
+//Parse.initialize("mainCadenceClap1");
+//Parse.serverURL = 'https://parsetestserver.herokuapp.com/parse'
 Parse.Cloud.define("processTimes", function (request, response) {
-    let testClapTimes = [1469596205, 1469596221, 1469596234];
+    let fileData = request.params.fileData;
     console.log(request);
-    console.log("---");
-    let requestFileInfo = request["params"]["data"];
-    console.log(requestFileInfo);
-    let result = processTimes(testClapTimes, requestFileInfo);
-    response.success(result);
+    processRequest(fileData, "clapProjs", response);
 });
 //# sourceMappingURL=app.js.map
